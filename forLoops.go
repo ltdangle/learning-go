d90e7372dcf9da2651b7478c6f7476c2a58203bb
Evgeniy Fedorov <mailbox@z01d.net>
2023-01-30 11:02:32 +0200

    forLoops.go

---
+++ forLoops.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Traditional for loop
+	for i := 0; i < 10; i++ {
+		fmt.Print(i*i, " ")
+	}
+	fmt.Println()
+
+	// While loop
+	i := 0
+	for {
+		if i == 10 {
+			break
+		}
+		fmt.Print(i*i, " ")
+		i++
+	}
+	fmt.Println()
+
+	// foreach loop
+	aSlice := []int{-1, 2, 1, -1, 2, -2}
+	for i, v := range aSlice {
+		fmt.Println("index:", i, "value: ", v)
+	}
+}
