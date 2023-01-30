7034ac3f7b9611b873c7c52e47026352fe1e5766
Evgeniy Fedorov <mailbox@z01d.net>
2023-01-30 11:09:29 +0200

    input.go

---
+++ input.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// get user input
+	fmt.Printf("Please give your name: ")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Println("Your name is", name)
+
+}
