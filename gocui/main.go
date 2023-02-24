eca0448e6d401e6bccce1b762f75e2dd60ea7a0e
Evgeniy Fedorov <mailbox@z01d.net>
2023-02-24 11:00:36 +0200

    WIP

--- gocui/main.go
+++ gocui/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	viewArr = []string{"v1", "v2", "v3","v4"}
+	viewArr = []string{"v1", "v2", "v3", "v4"}
 	active  = 0
 )
 
@@ -48,17 +48,31 @@ func nextView(g *gocui.Gui, v *gocui.View) error {
 }
 
 func layout(gui *gocui.Gui) error {
+	// window size
 	winX, winY := gui.Size()
-	logY := winY - 3
-	accountsX := winX / 10 * 2
-	emailsX := winX / 10 * 5
-	previewX := winX
+	// bottom log view
+	logStartX := 0
+	logStartY := winY - 3
+	logEndX := winX - 1
+	logEndY := winY - 1
+	// main view 'wrapper' (contains accounts, emails, preview)
+	mainViewStartY := 0
+	mainViewEndY := logStartY - 1
+	// accounts view
+	accountsStartX := 0
+	accountsEndX := (winX / 10 * 2) - 1
+	// emails list view
+	emailsStartX := accountsEndX + 1
+	emailsEndX := winX / 10 * 5
+	// email preview view
+	previewStartX := emailsEndX
+	previewEndX := winX
 
-	if accountsV, err := gui.SetView("v1", 0, 0, accountsX-1, logY-1); err != nil {
+	if accountsV, err := gui.SetView("v1", accountsStartX, mainViewStartY, accountsEndX, mainViewEndY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		accountsV.Title = strconv.Itoa(accountsX) + " Accounts"
+		accountsV.Title = strconv.Itoa(accountsStartX) + " - " + strconv.Itoa(accountsEndX) + " Accounts"
 		accountsV.Editable = true
 		accountsV.Wrap = true
 
@@ -67,27 +81,27 @@ func layout(gui *gocui.Gui) error {
 		}
 	}
 
-	if emailsV, err := gui.SetView("v2", accountsX, 0, emailsX-1, logY-1); err != nil {
+	if emailsV, err := gui.SetView("v2", emailsStartX, 0, emailsEndX-1, logStartY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		emailsV.Title = strconv.Itoa(emailsX) + " Emails"
+		emailsV.Title = strconv.Itoa(emailsStartX) + " - " + strconv.Itoa(emailsEndX) + " Emails"
 		emailsV.Wrap = true
 		emailsV.Autoscroll = true
 		emailsV.Editable = true
 	}
-	if v3, err := gui.SetView("v3", emailsX, 0, previewX-1, logY-1); err != nil {
+	if v3, err := gui.SetView("v3", previewStartX, 0, previewEndX-1, logStartY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		v3.Title = strconv.Itoa(previewX) + " Preview"
+		v3.Title = strconv.Itoa(previewEndX) + " Preview"
 		v3.Editable = true
 	}
-	if v4, err := gui.SetView("v4", 0, logY, winX-1, winY-1); err != nil {
+	if v4, err := gui.SetView("v4", logStartX, logStartY, logEndX, logEndY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		v4.Title = strconv.Itoa(previewX) + " Log"
+		v4.Title = strconv.Itoa(winX-1) + " Log"
 		v4.Editable = true
 	}
 	return nil
