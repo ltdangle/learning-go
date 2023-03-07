cd7aff2f2e8cf0d61695f223d4689698bbf61203
Evgeniy Fedorov <mailbox@z01d.net>
2023-03-07 13:33:57 +0200

    Tests setup()

--- gocui/store/store_test.go
+++ gocui/store/store_test.go
@@ -1,14 +1,19 @@
 package store
 
 import (
+	"learngocui/model"
 	"learngocui/repository"
 	"testing"
 )
 
-func TestInitialValues(t *testing.T) {
+func setup() (*Store, []model.EmailAccount) {
 	store := newStore()
 	seed := repository.SeedData()
 	store.setAccounts(seed)
+	return store, seed
+}
+func TestInitialValues(t *testing.T) {
+	store, seed := setup()
 	AssertEqual(t, store.selectedEmail, &seed[0].Emails[0])
 
 }
