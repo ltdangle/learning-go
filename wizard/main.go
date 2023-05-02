56a416989e61d818e7449635f9c94ad7c6b194fd
Evgeniy Fedorov <mailbox@z01d.net>
2023-05-02 15:00:23 +0300

    loaded!

--- wizard/main.go
+++ wizard/main.go
@@ -6,7 +6,15 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct{}
+type model struct {
+	questions []string
+	width     int
+	height    int
+}
+
+func New(questions []string) *model {
+	return &model{questions: questions}
+}
 
 func (m model) Init() tea.Cmd {
 	return nil
@@ -14,6 +22,9 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
@@ -25,17 +36,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return "Hello world!"
+	if m.width == 0 {
+		return "loading.."
+	}
+	return "loaded!"
 }
 
 func main() {
+	questions := []string{"what is your name?", "what is your favorite editor?", "what is your favorite quote?"}
+	m := New(questions)
+
 	f, err := tea.LogToFile("debug.log", "debug")
 	if err != nil {
-		log.Fatalf("err:%w", err)
+		log.Fatal(err)
 	}
 	defer f.Close()
 
-	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
